e57b637ed7edc5308125a2bce670a13f8594cc24
Masayuki Naito <mnaito@c5r4s3.42tokyo.jp>
2025-07-25 18:27:58 +0900

    modified bug

--- wordle.py
+++ wordle.py
@@ -36,6 +36,8 @@ print_info()
 words = get_words()
 answer = random.choice(words)
 
+print(answer)
+
 attempt = 1
 while attempt <= 6:
 	user_guess = input(f"挑戦 {attempt}: 推測を入力してください: ")
@@ -49,7 +51,7 @@ while attempt <= 6:
 		print(f"単語： {user_guess.upper()}")
 		feedback = get_feedback(answer, user_guess)
 		print(f"\t{feedback}\n")
-		if feedback == "OOOOO":
+		if feedback == "O O O O O":
 			print(f"おめでとうございます！ {attempt}回で単語'{answer.upper()}'を見つけました！")
 			break
 
